b3d1110a34df90e71dcd6d52980e83d8ae1deb63
EyesChain <46338289+EyesChain@users.noreply.github.com>
2019-01-11 14:52:38 +0900

    Update deploy_contracts.js
    constracts => contracts Change folder

--- migrations/deploy_contracts.js
+++ migrations/deploy_contracts.js
@@ -1,4 +1,4 @@
-var Eyestoken = artifacts.require("./constracts/Eyestoken.sol");
+var Eyestoken = artifacts.require("./contracts/Eyestoken.sol");
 
 module.exports = function(deployer, network, accounts) {
   deployer.deploy(Eyestoken, 'EYE', 'Eyestoken');
